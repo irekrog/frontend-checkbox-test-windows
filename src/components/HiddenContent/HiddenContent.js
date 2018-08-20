ff06dfec3d8b38e2f552d2a3ccea9ad3eb36df16
Irek Róg <irekfrontend@gmail.com>
2018-08-20 11:39:30 +0200

    checbkox logic and styles

---
+++ src/components/HiddenContent/HiddenContent.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './content.scss'
+
+const HiddenContent = () => <div className="text-content">Hidden Content</div>;
+
+export default HiddenContent;
--- src/modules/checkbox/index.js
+++ src/modules/checkbox/index.js
@@ -1,16 +1,33 @@
 /* eslint-disable */
 import React from 'react';
+import HiddenContent from '../../components/HiddenContent/HiddenContent';
 
 class Checkbox extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            checked: false
+        }
+    }
+
+    handleCheckbox = (event) => {
+        this.setState({
+            checked: event.target.checked
+        })
+    }
+
     render() {
+        console.log(this.state.checked);
         return(
             <div className="checkbox">
                 <div className="title">
                     Checkbox
                 </div>
                 <div className="content">
-                    <input type="checkbox" />
+                    <input className="switch" type="checkbox" onChange={this.handleCheckbox} />
                 </div>
+                {this.state.checked && <HiddenContent />}
             </div>
         );
     }
